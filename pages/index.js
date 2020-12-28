4ef69ed922143d7175c0aac4de4dec0d4d90890f
Vishhvak Srinivasan <vishhvak@outlook.com>
2020-12-28 21:33:09 +0530

    Feat: Add basic stuff

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Golden Hour</title>
+        <title>Golden Hour - Aira</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className={styles.home}>
+        <img src="/artist.jpg"></img>
+        <h1>Golden Hour</h1>
+        <h2>Aira</h2>
+      </div>
     </div>
   );
 }
